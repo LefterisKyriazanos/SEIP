f368be8573124e51854c3dc640a57cbc0c651d00
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-04-12 22:04:56 +0300

    Added proper tittles for x and y axis. I also added a Chart tittle. This commit resolves issue #6

--- gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
+++ gradeshistogram/src/main/java/gradeshistogram/HistogramGenerator.java
@@ -88,7 +88,8 @@ public class HistogramGenerator {
 		boolean urls = false; // do not visualize urls
 
 		// Declare and initialize a createXYLineChart JFreeChart
-		JFreeChart chart = ChartFactory.createXYLineChart("Chart title", "x_axis title", "y_axis_title", dataset,
+		JFreeChart chart = ChartFactory.createXYLineChart(
+				"Student Grades Histogram", "Student ID", "Grade", dataset,
 				PlotOrientation.VERTICAL, legend, tooltips, urls);
 
 		/*
