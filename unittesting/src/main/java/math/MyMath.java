67b049a8132e11beed5f27900c66936bf01f905e
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-05-04 01:49:22 +0300

    created the MyMath class that contains the factorial method, which calculates the factorial of number n

---
+++ unittesting/src/main/java/math/MyMath.java
@@ -0,0 +1,29 @@
+package math;
+
+/**
+ * @author LefterisKyriazanos
+ * @version 1.0
+ */
+public class MyMath {
+
+	/**
+	 * Finds the factorial of an integer number (n).
+	 *
+	 * @param n
+	 *              the integer which factorial we want to calculate.
+	 * @return factorial the factorial of number n.
+	 * @exception IllegalArgumentException
+	 *                                         when n < 0 or n > 12.
+	 *
+	 */
+	public int factorial(int n) {
+		if (n < 0 || n > 12)
+			throw new IllegalArgumentException("n should be between 0 and 12");
+		int factorial = 1;
+		for (int i = 2; i <= n; ++i) {
+			factorial *= i;
+		}
+		return factorial;
+	}
+
+}
