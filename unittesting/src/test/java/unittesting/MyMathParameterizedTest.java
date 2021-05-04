76a6bdbe5f46163116beda0cc4589c7f09e20548
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-05-04 18:19:06 +0300

    added the MyMathParameterizedTest class to test the MyMath factorial method

---
+++ unittesting/src/test/java/unittesting/MyMathParameterizedTest.java
@@ -0,0 +1,37 @@
+package unittesting;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import math.MyMath;
+
+@RunWith(Parameterized.class)
+public class MyMathParameterizedTest {
+
+	@Parameter(value = 0)
+	public int n;
+
+	@Parameter(value = 1)
+	public int factorial;
+
+	MyMath mm = new MyMath();
+
+
+	@Parameters
+	public static Iterable<Object[]> data() {
+		return Arrays.asList(
+				new Object[][]{{0, 1}, {1, 1}, {2, 2}, {12, 479001600}});
+	}
+
+	@Test
+	public void test_factorial() {
+
+		Assert.assertEquals(factorial, mm.factorial(n));
+	}
+}
