dd7686cc8a1a05b014f01650eee0855707fd22d1
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-05-04 18:47:09 +0300

    created the MyMathTestSuite that groups the test classes MyMathTest and MyMathParameterizedTest

---
+++ unittesting/src/test/java/unittesting/MyMathTestSuite.java
@@ -0,0 +1,13 @@
+package unittesting;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MyMathTest.class, MyMathParameterizedTest.class})
+public class MyMathTestSuite {
+
+	// this class is empty. It's used as a holder
+	// of the above annotations
+
+}
