52c29250da212fa3dd704975cab9c8068404a1df
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-05-05 21:29:56 +0300

    removed the unnecessary method for testing isPrime, which was placed incorrectly

---
+++ unittesting/src/test/java/unittesting/MyMathIsPrimeParameterizedTest.java
@@ -0,0 +1,13 @@
+package unittesting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyMathIsPrimeParameterizedTest {
+
+	@Test
+	public void test_isPrime() {
+
+		Assert.assertEquals(factorial, mm.factorial(n));
+	}
+}
