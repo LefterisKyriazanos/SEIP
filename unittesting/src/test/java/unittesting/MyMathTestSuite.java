3a5da40c63409f893658f7248c9840899d56d2fd
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-05-05 22:39:32 +0300

    added the MyMathIsPrimeParameterizedTest in the MyMathTestSuite

--- unittesting/src/test/java/unittesting/MyMathTestSuite.java
+++ unittesting/src/test/java/unittesting/MyMathTestSuite.java
@@ -4,7 +4,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({MyMathTest.class, MyMathFactorialParameterizedTest.class})
+@Suite.SuiteClasses({MyMathTest.class, MyMathFactorialParameterizedTest.class,
+		MyMathIsPrimeParameterizedTest.class})
 public class MyMathTestSuite {
 
 	// this class is empty. It's used as a holder
