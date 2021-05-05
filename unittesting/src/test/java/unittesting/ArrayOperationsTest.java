953596f7c4aacd04031ce0f3d896fad952d6c221
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-05-06 02:39:40 +0300

    did some bug fixing regarding when declarations at lines 22-27. Now the test compiles and runs successfully

--- unittesting/src/test/java/unittesting/ArrayOperationsTest.java
+++ unittesting/src/test/java/unittesting/ArrayOperationsTest.java
@@ -1,3 +1,4 @@
+
 package unittesting;
 import static org.mockito.Mockito.*;
 
@@ -10,20 +11,20 @@ import math.MyMath;
 
 public class ArrayOperationsTest {
 
-	ArrayOperations ao;
+	ArrayOperations ao = new ArrayOperations();
 
 	@Test
 	public void test_ArrayOperations_Mock() {
 		MyMath mm = mock(MyMath.class);
 		FileIO fio = mock(FileIO.class);
 		String path = "./src/test/resources/mocking_file.txt";
-		
-		when(fio.readFile(path), thenReturn(new int[]{2, 6, 19, 22, 23}));
-		when(mm.isPrime(2),thenReturn(true));
-		when(mm.isPrime(6),thenReturn(false));
-		when(mm.isPrime(19),thenReturn(true));
-		when(mm.isPrime(22),thenReturn(false));
-		when(mm.isPrime(23),thenReturn(true));
+		int [] array = {2, 6, 19, 22, 23};
+		when(fio.readFile(path)).thenReturn(array);
+		when(mm.isPrime(2)).thenReturn(true);
+		when(mm.isPrime(6)).thenReturn(false);
+		when(mm.isPrime(19)).thenReturn(true);
+		when(mm.isPrime(22)).thenReturn(false);
+		when(mm.isPrime(23)).thenReturn(true);
 
 		Assert.assertArrayEquals(new int[]{2, 19, 23},
 				ao.findPrimesInFile(fio, path, mm));
