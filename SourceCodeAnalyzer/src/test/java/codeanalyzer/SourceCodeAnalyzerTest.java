beddc80193ca04b2b16447f95af3f326e4ca42b6
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-06-11 23:49:00 +0300

    Update test methods

--- SourceCodeAnalyzer/src/test/java/codeanalyzer/SourceCodeAnalyzerTest.java
+++ SourceCodeAnalyzer/src/test/java/codeanalyzer/SourceCodeAnalyzerTest.java
@@ -1,56 +1,59 @@
 package codeanalyzer;
 
-import java.io.File;
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
-import codeanalyzer.SourceCodeAnalyzer;
-
-import static org.junit.Assert.*;
+// This test class depends on the functionality of codeanalyzer.SourceFileReader class
 
 public class SourceCodeAnalyzerTest {
 	private static List<String> testSourceCode;
+	private static String sourceCode;
 	private final static String TYPE_REGEX = "regex";
 	private final static String TYPE_STRCOMP = "strcomp";
 	private final static String TEST_CLASS = "src/test/resources/TestClass.java";
-	private SourceCodeAnalyzer sca = new SourceCodeAnalyzer("local");
+	private SourceCodeAnalyzer sca = new SourceCodeAnalyzer();
+	private static SourceFileReader sfr = new SourceFileReader();
+
+	@BeforeClass
+	public static void setUp() throws IOException {
+		testSourceCode = sfr.readLocalFileIntoList(TEST_CLASS);
+		sourceCode = sfr.readLocalFileIntoString(TEST_CLASS);
+	}
 	
 	
 	@Test
 	public void testCalculateRegexLOC() throws IOException {
-		assertEquals(21, sca.calculateLOC(TEST_CLASS, TYPE_REGEX));
+		assertEquals(21, sca.calculateRegexLOC(sourceCode));
 	}
 	
 	@Test
 	public void testCalculateStrCompLOC() throws IOException {
-		assertEquals(7, sca.calculateLOC(TEST_CLASS, TYPE_STRCOMP));
+		assertEquals(7, sca.calculateStringLOC(testSourceCode));
 	}
 	
 	@Test
 	public void testCalculateRegexNOM() throws IOException {
-		assertEquals(3, sca.calculateNOM(TEST_CLASS, TYPE_REGEX));
+		assertEquals(3, sca.calculateRegexNOM(sourceCode));
 	}
 	
 	@Test
 	public void testCalculateStrCompNOM() throws IOException {
-		assertEquals(3, sca.calculateNOM(TEST_CLASS, TYPE_STRCOMP));
+		assertEquals(3, sca.calculateStringNOM(testSourceCode));
 	}
 	
 	@Test
 	public void testCalculateRegexNOC() throws IOException {
-		assertEquals(3, sca.calculateNOC(TEST_CLASS, TYPE_REGEX));
+		assertEquals(3, sca.calculateRegexNOC(sourceCode));
 	}
 	
 	@Test
 	public void testCalculateStrCompNOC() throws IOException {
-		assertEquals(3, sca.calculateNOC(TEST_CLASS, TYPE_STRCOMP));
+		assertEquals(3, sca.calculateStringNOC(testSourceCode));
 	}
 	
 }
