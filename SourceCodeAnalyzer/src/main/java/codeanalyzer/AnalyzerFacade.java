c78b90e024bd85e5410e8521d768bec2303d61d9
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-06-12 00:05:31 +0300

    Add if else statements that check given file location and add proper error message for invalid input

--- SourceCodeAnalyzer/src/main/java/codeanalyzer/AnalyzerFacade.java
+++ SourceCodeAnalyzer/src/main/java/codeanalyzer/AnalyzerFacade.java
@@ -12,28 +12,44 @@ public class AnalyzerFacade {
 			String outputFilePath, String outputFileType) throws IOException {
 
 		SourceFileReader fileReader = new SourceFileReader();
-		String sourceCode;
-		List<String> sourceCodeList;
+		String sourceCode = null;
+		List<String> sourceCodeList = null;
 		SourceCodeAnalyzer analyzer = new SourceCodeAnalyzer();
 		int loc = -1;
 		int nom = -1;
 		int noc = -1;
 
 		if (scaType.equals("regex")) {
-			sourceCode = fileReader.readFileIntoString(filePath, location);
+			if (location.equals("local")) {
+				sourceCode = fileReader.readLocalFileIntoString(filePath);
+			} else if (location.equals("web")) {
+				sourceCode = fileReader.readWebFileIntoString(filePath);
+			} else {
+				System.err.println("Invalid Source Code Location Type. Try web OR local");
+				System.exit(1);
+			}
+
 			loc = analyzer.calculateRegexLOC(sourceCode);
 			nom = analyzer.calculateRegexNOM(sourceCode);
 			noc = analyzer.calculateRegexNOC(sourceCode);
 		} else if (scaType.equals("strcomp")) {
-			sourceCodeList = fileReader.readFileIntoList(filePath, location);
+			
+
+			if (location.equals("local")) {
+				sourceCodeList = fileReader.readLocalFileIntoList(filePath);
+			} else if (location.equals("web")) {
+				sourceCodeList = fileReader.readWebFileIntoList(filePath);
+			} else {
+				System.err.println("Invalid Source Code Location Type. Try web OR local");
+				System.exit(1);
+			}
+				
+				
 			loc = analyzer.calculateStringLOC(sourceCodeList);
 			nom = analyzer.calculateStringNOM(sourceCodeList);
 			noc = analyzer.calculateStringNOC(sourceCodeList);
-		} else {
-			System.err.println("Invalid Arguments");
-			System.exit(1);
+
 		}
-		
 		if (loc != -1 && nom != -1 && noc != -1) {
 			Map<String, Integer> metrics = new HashMap<>();
 			metrics.put("loc", loc);
@@ -43,6 +59,6 @@ public class AnalyzerFacade {
 			exporter.writeFile(outputFileType, metrics, outputFilePath);
 		}
 
-	}
 
+	}
 }
