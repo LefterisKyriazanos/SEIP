ee0d41096255b26bd95cac366acab819dd054461
Lefteris Kyriazanos <lefteriskyriazanos@gmail.com>
2021-06-11 21:55:27 +0300

    Split and restruct readFileIntoString and readFileIntoList methods to four new ones and add proper JavaDoc

--- SourceCodeAnalyzer/src/main/java/codeanalyzer/SourceFileReader.java
+++ SourceCodeAnalyzer/src/main/java/codeanalyzer/SourceFileReader.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.FileNotFoundException;
 
 /**
  * Retrieves (reads) the contents of a given file.
@@ -22,77 +23,133 @@ public class SourceFileReader {
 
 	/**
 	 * Reads a file and returns its content in a List
-	 * @param fileReaderType the location of a file 
-	 * (<b>local</b> for locally stored files, 
-	 * <b>web</b> for files stored on the web). 
-	 * @param filepath the url of the file
+	 * @param filepath the location of a locally stored file
 	 * @return a List that contains the contents of the file 
-	 * or null if the type is neither <b>local</b> nor <b>web</b>
-	 * @throws IOException
+	 * @throws IOException and FileNotFoundException
 	 */
-	public List<String> readFileIntoList(String filepath, String type) throws IOException {
-		// read a locally stored file
-		if (type.equals("local")) {
+	public List<String> readLocalFileIntoList(String filepath) throws FileNotFoundException, IOException {
+		
+		// declare bufferedReader
+		BufferedReader reader = null;
+		
+		try {
 			List<String> lines = new ArrayList<>();
 			File file = new File(filepath);
-			BufferedReader reader = new BufferedReader(new FileReader(file));
+			reader = new BufferedReader(new FileReader(file));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
 				lines.add(line);
 			}
 			reader.close();
-			return lines;
-		// read a file stored in the web
-		} else if (type.equals("web")) {
+			return lines; 
+			
+		} catch (FileNotFoundException ex) {
+			System.err.println("File does not exist. Please give a valid path.");
+			System.exit(1);
+		} catch (IOException ex2) {
+			System.err.println(ex2);
+			System.exit(1);
+		}
+	}
+			
+		
+	/**
+	 * Reads a file and returns its content in a List
+	 * @param filepath the URL of a file stored in the web.
+	 * @return a List that contains the contents of the file 
+	 * @throws IOException and FileNotFoundException
+	 */
+	public List<String> readWebFileIntoList(String filepath) throws FileNotFoundException, IOException {
+		
+		// declare bufferedReader
+		BufferedReader reader = null;
+		
+		try {
+			// read a file stored in the web
 			List<String> lines = new ArrayList<>();
 	        URL url = new URL(filepath);
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+	        reader = new BufferedReader(new InputStreamReader(url.openStream()));
 	        String line = null;
 	        while ((line = reader.readLine()) != null) {
 	        	lines.add(line);
 	        }
 	        reader.close();
 			return lines;
-		} else {
-			return null;
+			
+		} catch (FileNotFoundException ex) {
+			System.err.println("File does not exist. Please give a valid path.");
+			System.exit(1);
+		} catch (IOException ex2) {
+			System.err.println(ex2);
+			System.exit(1);
 		}
+			
+		
 	}
 	
 	/**
-	 * Reads a file and returns its content in a single String
-	 * @param fileReaderType the location of a file 
-	 * (<b>local</b> for locally stored files, 
-	 * <b>web</b> for files stored on the web). 
-	 * @param filepath the url of the file
-	 * @return a String that contains the contents of the file
-	 * or null if the type is neither <b>local</b> nor <b>web</b>
-	 * @throws IOException
+	 * Reads a file and returns its content in a String
+	 * @param filepath the location of a locally stored file.
+	 * @return a String that contains the contents of the file 
+	 * @throws IOException and FileNotFoundException
 	 */
-	public String readFileIntoString(String filepath, String type) throws IOException {
-		// read a locally stored file
-		if (type.equals("local")) {
+	public String readLocalFileIntoString(String filepath) throws FileNotFoundException, IOException {
+		
+		// declare bufferedReader
+		BufferedReader reader = null;
+		
+		try {
+			// read a locally stored file
 			StringBuilder sb = new StringBuilder();
 			File file = new File(filepath);
-			BufferedReader reader = new BufferedReader(new FileReader(file));
+			reader = new BufferedReader(new FileReader(file));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
 				sb.append(line + "\n");
 			}
 			reader.close();
 			return sb.toString();
-		// read a file stored in the web
-		} else if (type.equals("web")) {
+			
+		} catch (FileNotFoundException ex) {
+			System.err.println("File does not exist. Please give a valid path.");
+			System.exit(1);
+		} catch (IOException ex2) {
+			System.err.println(ex2);
+			System.exit(1);
+		}
+	}		
+			
+			
+	/**
+	 * Reads a file and returns its content in a String
+	 * @param filepath the URL of a file stored in the web.
+	 * @return a String that contains the contents of the file 
+	 * @throws IOException and FileNotFoundException
+	 */
+	public String readWebFileIntoString(String filepath) throws FileNotFoundException, IOException {
+		
+		// declare bufferedReader
+		BufferedReader reader = null;
+		
+		try {
+			
+			// read a file stored in the web
 			StringBuilder sb = new StringBuilder();
 	        URL url = new URL(filepath);
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+	        reader = new BufferedReader(new InputStreamReader(url.openStream()));
 	        String line = null;
 	        while ((line = reader.readLine()) != null) {
 	        	sb.append(line + "\n");
 	        }
 	        reader.close();
 			return sb.toString();
-		} else {
-			return null;
+			
+		} catch (FileNotFoundException ex) {
+			System.err.println("File does not exist. Please give a valid path.");
+			System.exit(1);
+		} catch (IOException ex2) {
+			System.err.println(ex2);
+			System.exit(1);
 		}
 	}
 
